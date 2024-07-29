263d2bfde77e2d1d57b4a9d26b66b5e3d8666fba
TECH\jjrme <jjrsmedeiros@gmail.com>
2024-07-29 02:32:16 -0300

    docs: configurando swagger

--- adote-api/src/main/java/br/com/tech/adote/AdoteApplication.java
+++ adote-api/src/main/java/br/com/tech/adote/AdoteApplication.java
@@ -1,8 +1,11 @@
 package br.com.tech.adote;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "Adote API", version = "1.0.0", description = "API de gestão de adoção de animais"))
 @SpringBootApplication
 public class AdoteApplication {
 
