7cda93c64a1abadd60eedf09202f3d3b7cb6a606
TECH\jjrme <jjrsmedeiros@gmail.com>
2024-07-29 02:42:53 -0300

    feat: criando endpoints no controller

---
+++ adote-api/src/main/java/br/com/tech/adote/api/controller/AnimalController.java
@@ -0,0 +1,57 @@
+package br.com.tech.adote.api.controller;
+
+import br.com.tech.adote.api.assembler.AnimalAssembler;
+import br.com.tech.adote.api.model.AnimalInput;
+import br.com.tech.adote.api.model.AnimalModel;
+import br.com.tech.adote.domain.enums.Status;
+import br.com.tech.adote.domain.model.Animal;
+import br.com.tech.adote.domain.service.AnimalService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/animals")
+public class AnimalController {
+
+    private final AnimalService animalService;
+    private final AnimalAssembler animalAssembler;
+
+    @Operation(summary = "Cria registro do Animal", description = "Metodo que cria registro do Animal", tags = "Criar")
+    @PostMapping
+    public ResponseEntity<AnimalModel> createAnimal(@Valid @RequestBody AnimalInput animalInput) {
+        Animal novoAnimal = animalAssembler.toEntity(animalInput);
+        return new ResponseEntity<>(animalService.createAnimal(novoAnimal), HttpStatus.CREATED);
+    }
+
+    @Operation(summary = "Lista todos", description = "Metodo que retorna todos os animais", tags = "Consultar")
+    @GetMapping
+    public ResponseEntity<List<AnimalModel>> getAllAnimals() {
+        return new ResponseEntity<>(animalService.getAllAnimals(), HttpStatus.OK);
+    }
+
+    @Operation(summary = "Busca por ID", description = "Metodo que consulta o animal pelo ID", tags = "Consultar")
+    @GetMapping("/{id}")
+    public ResponseEntity<AnimalModel> getAnimalById(@PathVariable Long id) {
+        return ResponseEntity.ok(animalService.getAnimalById(id));
+    }
+
+    @Operation(summary = "Busca por Nome", description = "Metodo que consulta o animal pelo Nome", tags = "Consultar")
+    @GetMapping("/name")
+    public ResponseEntity<List<AnimalModel>> getAnimalByName(@RequestParam String name) {
+        return ResponseEntity.ok(animalService.getAnimalsByName(name));
+    }
+
+    @Operation(summary = "Altera Status", description = "Metodo que altera o status do animal", tags = "Alterar")
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<Animal> updateAnimalStatus(@PathVariable Long id, @RequestParam Status status) {
+        return ResponseEntity.ok(animalService.updateAnimalStatus(id, status));
+    }
+
+}
